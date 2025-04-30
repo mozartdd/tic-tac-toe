a2a5e14a44e522f35878e67da04e3d9171be2f2e
Dainis Dilevka <mozart_dd@outlook.com>
2025-04-30 09:51:22 +0100

    add iife capsule, gameobard and winnable cells

---
+++ script.js
@@ -0,0 +1,27 @@
+(() => {
+    function GameFlow() {
+        //Board to hold cells/fields selected by user's
+        const gameBoard = Array(9).fill('');
+        //Shallow copy of gameBoardArray
+        const gameBoardCopy = [...gameBoard];
+
+        const WINNABLE_CELLS = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+
+            [0, 4, 8], [2, 4, 6] //diagonal
+        ];
+
+        
+
+        return {}
+    }
+    return {GameFlow};
+})();
+
+
+
+
+
+
