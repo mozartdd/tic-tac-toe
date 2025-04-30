4697908ca3344dc42c3b50e95bdf6776794717ca
Dainis Dilevka <mozart_dd@outlook.com>
2025-04-30 13:07:46 +0100

    add function to chechk for winning combination

--- script.js
+++ script.js
@@ -13,12 +13,33 @@
             [0, 4, 8], [2, 4, 6] //diagonal
         ];
 
-        
+        const playerMoves = {
+            player1: [],
+            player2: []
+        };
 
-        return {}
+        //Function to add user checked symbol to game board
+        function makeMove(index, symbol) {
+            gameBoard[index] = symbol;
+        };
+        //Function that loops trough all winnable combinations and for each cell compares that all indices is included in player moves
+        function checkForWinningCombination(player, winCells) {
+            return winCells.some((cells) => cells.every((cell ) => player.includes(cell)));
+        }
+
+
+
+        return {makeMove, checkForWinningCombination};
+    }
+
+    function GameLogic() {
+            
     }
     return {GameFlow};
 })();
+
+
+
 
 
 
