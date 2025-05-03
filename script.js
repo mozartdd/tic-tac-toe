b9bdc2a48635f0370f0549207b8d79e20db30927
Dainis Dilevka <mozart_dd@outlook.com>
2025-05-03 10:47:29 +0100

    add function to push player symbol to gameBoard array

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ const globalWrapper = (() => {
 
         const WINNABLE_CELLS = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
-
             [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
-
             [0, 4, 8], [2, 4, 6] //diagonal
         ];
         //Obj that hold information of player chosen cells inside of an arrays
@@ -21,35 +19,45 @@ const globalWrapper = (() => {
         //Function to add user checked symbol to game board
         function makeMove(index, symbol) {
             gameBoard[index] = symbol;
+            console.log(gameBoard);
         };
         //Function that loops trough all winnable combinations and for each cell compares that all indices is included in player moves
         function checkForWinningCombination(player) {
             return WINNABLE_CELLS.some((cells) => cells.every((cell ) => player.includes(cell)));
-        }
+        };
         //Function that resets board to initial state
         function resetBoard(board) {
             for(let i = 0; i < board.length; i++) {
                 board[i] = '';
             }
-        }
+        };
+        
+        //Function which will push user move to playerMoves obj
+        function pushPlayerMoves(player, move) {
+            playerMoves[player].push(move);
+        };
 
-        return {makeMove, checkForWinningCombination, resetBoard};
+        return {makeMove, checkForWinningCombination, resetBoard, pushPlayerMoves};
     }
 
     function GameLogic() {
         let isPlayerOneMove = true;
-        let isGameOver = false;
+        let isGameOver = true;
         let moveCount = 0;
 
         const player1Symbol = 'X';
         const player2Symbol = 'O';
 
-        //TODO: create a function which will push user move to playerMoves obj
-        function pushPlayerMoves(player, move) {
-            playerMoves[player].push(move);
-        }
+        //Function that adds player symbol to gameBoard obj based on player move
+        const pushSymbol = (move) => {
+            const currentSymbol = isPlayerOneMove ? player1Symbol : player2Symbol;
+            gameFlow.makeMove(move, currentSymbol);
+            isPlayerOneMove = !isPlayerOneMove;  // Toggles the turn
+        };
 
-
+        
+        
+        return {pushSymbol};
     }
     //TODO: create design and ui before start working on logic
     function DomManipulations() {
@@ -60,3 +68,7 @@ const globalWrapper = (() => {
 
 const gameFlow = globalWrapper.GameFlow();
 const gameLogic = globalWrapper.GameLogic();
+
+gameLogic.pushSymbol(0);
+gameLogic.pushSymbol(1);
+gameLogic.pushSymbol(2);
